2b52e9bbd6705dc0d2623cdd5b0f2aca253f201b
Artur Alexandre <arturalexandredemelo@gmail.com>
2022-03-24 17:26:45 -0300

    Adicionando paginação e cache.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -9,6 +9,13 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,6 +25,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -41,15 +49,18 @@ public class TopicosController {
 	@Autowired
 	private CursoRepository cursoRepository;
 	
-	@GetMapping
-	public List<TopicoDto> lista(String nomeCurso) {
+	@GetMapping		
+	@Cacheable(value = "listaDeTopicos") //esse value vai identificar o cache desse metodo         // se nao receber nenhum parametro na url, vai ordenar pelo id decrescente, a partir da pagina 0, 10 por pagina
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao) { //requestparam avisa pro spring que esses parametros sao de url, obrigatorio // required=false diz que o nome do curso eh opcional
+		
+				
 		if(nomeCurso == null) {
-			List<Topico> topicos = topicoRepository.findAll();
+			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			
 			return TopicoDto.converter(topicos);
 		}
 		else {	
-			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso); // utilizando findBy... é só criar o método no repository que o springdata gera a query automaticamente de acordo com o que vem depois do By
+			Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao); // utilizando findBy... é só criar o método no repository que o springdata gera a query automaticamente de acordo com o que vem depois do By
 			
 			return TopicoDto.converter(topicos);
 				
@@ -57,6 +68,7 @@ public class TopicosController {
 	}
 	@PostMapping
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)  // vai limpar o cache pra poder ser atualizado
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) { // quando é Get, o parametro vem na url, quando é Post, tem que avisar ao spring que o parametro vem no corpo
 																														// o @valid  vai validar o que vem do topicoform, que a gente anotou com os requisitos
 		Topico topico = form.converter(cursoRepository);
@@ -77,6 +89,7 @@ public class TopicosController {
 	}
 	@PutMapping("/{id}")
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if(optional.isPresent()) {
@@ -90,6 +103,7 @@ public class TopicosController {
 	}
 	@DeleteMapping("/{id}")
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> remover(@PathVariable Long id) {
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if(optional.isPresent()) {
--- src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
@@ -4,6 +4,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import br.com.alura.forum.modelo.Topico;
 
 // dto vai servir para retornar so alguns atributos escolhidos
@@ -34,8 +37,8 @@ public class TopicoDto {
 		return dataCriacao;
 	}
 	
-	public static List<TopicoDto> converter(List<Topico> topicos){
-		return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
+	public static Page<TopicoDto> converter(Page<Topico> topicos){
+		return topicos.map(TopicoDto::new);
 	}
 
 }
