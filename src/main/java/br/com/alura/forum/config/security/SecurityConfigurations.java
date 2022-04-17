6d78d81fe0c6af012f6dd051569de33d75613e5e
Shadwz1NN <arturalexandredemelo@gmail.com>
2022-04-16 22:25:21 -0300

    Adicionando auntenticação via JWT

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,18 +1,38 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import br.com.alura.forum.repository.UsuarioRepository;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	@Bean // para trabalhar com autenticacao jwt tem que sobrescrever esse metodo aqui para poder injetar o AuthenticationManager em outra classe
+	protected AuthenticationManager authenticationManager() throws Exception {
+		
+		return super.authenticationManager();
+	}
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository; 
+	
+	@Autowired
+	private TokenService token;
 	
 	@Autowired
 	private AutenticacaoService autenticacaoService;
@@ -26,9 +46,12 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.antMatchers(HttpMethod.GET, "/topicos").permitAll() //vai permitir o get do endereco /topicos
-			.antMatchers(HttpMethod.GET, "/topicos/*").permitAll() //se chegar uma requisicao get para topicos/alguma coisa ele vai permitir
+			.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+			.antMatchers(HttpMethod.POST, "/auth").permitAll()
 			.anyRequest().authenticated() // qualquer outra requisicao tem que autenticar; 
-			.and().formLogin();  
+			.and().csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and().addFilterBefore(new AutenticacaoViaTokenFilter(token, usuarioRepository), UsernamePasswordAuthenticationFilter.class);  
 	}
 	@Override // configuracoes de recursos estaticos(javascript, css, imagens, etc)
 	public void configure(WebSecurity web) throws Exception {
